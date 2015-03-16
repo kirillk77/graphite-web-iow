d48bf1d893216011fb2699f11177f4420529f1ea
Kirill Kondratenko <cybem@cybem.info>
2015-03-16 19:32:04 +0300

    Add the 'get_tenant' function

--- webapp/graphite/iow_util.py
+++ webapp/graphite/iow_util.py
@@ -1,5 +1,13 @@
 from django.conf import settings
 
+def get_tenant(request):
+  tenant = ""
+  if 'tenant' in request.GET:
+    tenant = request.GET['tenant']
+  else:
+    tenant = request.session['tenant']
+  return check_tenant(tenant)
+
 def check_tenant(tenant):
   if tenant not in settings.TENANT_LIST:
     return settings.TENANT_LIST[0]
